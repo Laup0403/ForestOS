63a75b75a8498e1eb5f34c423487b1c98c1ad328
Laupi_oida <laupi0403@proton.me>
2026-07-05 22:24:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
-function updateTime() {document.querySelector("#timeElement").innerHTML = new Date().toLocaleString();}
+//Uhr
+
+function updateTime() { document.querySelector("#timeElement").innerHTML = new Date().toLocaleString(); }
 setInterval(updateTime, 1000);
 
-// Make the DIV element draggable:
-dragElement(document.getElementById("welcome"));
-dragElement(document.getElementById("notes"))
+//Variablen
 
-// Step 1: Define a function called `dragElement` that makes an HTML element draggable.
+var biggestIndex = 1;
+var topBar = document.querySelector("#top")
+var selectedIcon = undefined
+var notescontent = [{
+  title: "Welcome",
+  date: "04.07.26",
+  content: `
+    <p contenteditable="True">
+    <strong>Welcome to Woodwriter</strong><br>
+    <br>
+    This is an app where i put my<i> literary gems</i><br>
+    <br>
+    I can even write <i> different Textstyles</i>, <br>
+    like a poem:
+    </p>
+    <blockquote>
+        <strong>The Cow</strong><br>
+        <small>Laupi_oida</small> <br>
+        I was on a field<br>
+        and there was a cow<br>
+        that mowed the lawn<br>
+        <br>
+        then i came near<br>
+        and it mowed my hair<br>
+        now im bald<br>
+        <br>
+        The End<br>
+    </blockquote>
+  `
+},
+{
+  title: "Future Plans",
+  date: "06.07.26",
+  content: `
+  <h2> Future plans:</h2>
+  <p>-a terminal <br><br>
+      -games <br><br>
+      -a better taskbar <br><br>
+      -a file manager <br><br>
+      -a browser
+  `
+},
+{
+  title: "Secret!",
+  date: "06.07.26",
+  content: `
+    <p contenteditable="True">
+    This is very serius:<br>
+    I live on Earth
+    </p>
+    `
+}
+]
+
+//Fenster verschiebbar machen
+
 function dragElement(element) {
   // Step 2: Set up variables to keep track of the element's position.
   var initialX = 0;
@@ -57,9 +112,7 @@ function dragElement(element) {
   }
 }
 
-function openWindow(element) {
-  element.style.display = "flex"
-}
+//Fenster schließen
 
 function closeWindow(window, closebutton) {
   var screen = document.querySelector("#" + window)
@@ -69,11 +122,15 @@ function closeWindow(window, closebutton) {
   })
 }
 
-closeWindow("welcome", "welcomeclose")
+//Fenster öffnen
 
-closeWindow("notes", "notesclose")
-
-var selectedIcon = undefined
+function openWindow(element) {
+  console.log("window opened")
+  element.style.display = "inline"
+  biggestIndex++;  // Increment biggestIndex by 1
+  element.style.zIndex = biggestIndex;
+  topBar.style.zIndex = biggestIndex + 1;
+}
 
 function selectIcon(element) {
   element.classList.add("selected");
@@ -81,6 +138,9 @@ function selectIcon(element) {
 }
 
 function deselectIcon(element) {
+  if (!element) {
+     return;
+   }
   element.classList.remove("selected");
   selectedIcon = undefined
 }
@@ -92,4 +152,60 @@ function handleIconTap(element) {
   } else {
     selectIcon(element)
   }
+}
+
+//Fenster nach oben kommen lassen
+
+function addWindowTapHandling(element) {
+  element.addEventListener("mousedown", () =>
+    handleWindowTap(element)
+  )
+}
+
+function handleWindowTap(element) {
+  biggestIndex++;  // Increment biggestIndex by 1
+  element.style.zIndex = biggestIndex;
+  topBar.style.zIndex = biggestIndex + 1;
+  deselectIcon(selectedIcon)
+}
+
+//alles auf einmal initialisieren
+
+function intializeWindow(windowName) {
+  var screen = document.querySelector("#" + windowName)
+  addWindowTapHandling(screen)
+  closeWindow(windowName, windowName + "close")
+  dragElement(screen)
+}
+
+intializeWindow("notes")
+intializeWindow("welcome")
+
+//Notes app
+
+function setNotesContent(index) {
+
+  var notesContent = document.querySelector("#notesContent")
+
+  notesContent.innerHTML = notescontent[index].content
+}
+
+setNotesContent(0)
+
+function addToSideBar(index) {
+  var sidebar= document.querySelector("#notessidebar");
+  var note = notescontent[index];
+  newDiv = document.createElement("div");
+  newDiv.innerHTML = `
+    <p style="margin: 0px;">${note.title}</p>
+    <p style="font-size: 12px; margin: 0px;">${note.date}</p>
+  `
+  newDiv.addEventListener("click", function () {
+    setNotesContent(index);
+  });
+  sidebar.appendChild(newDiv)
+}
+
+for (let i = 0; i < notescontent.length; i++) {
+  addToSideBar(i)
 }
